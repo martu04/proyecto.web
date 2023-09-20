c82a65cab9aa4ea9dabc69459e2d9e1871d10340
Martina Marquez <martina04juarez@gmail.com>
2023-09-20 17:44:56 -0300

    testing crontrollers

--- src/postres/postres.controller.spec.ts
+++ src/postres/postres.controller.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostresController } from './postres.controller';
 import { PostresService } from './postres.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Postre } from './entities/postre.entity';
+import { Receta } from 'src/recetas/entities/receta.entity';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 
 describe('PostresController', () => {
   let controller: PostresController;
@@ -9,6 +15,18 @@ describe('PostresController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostresController],
       providers: [PostresService],
+      imports:[TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [Postre, Receta],
+        synchronize: true,
+      }),
+      TypeOrmModule.forFeature([Postre]),
+    ] 
     }).compile();
 
     controller = module.get<PostresController>(PostresController);
--- src/recetas/recetas.controller.spec.ts
+++ src/recetas/recetas.controller.spec.ts
@@ -1,6 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecetasController } from './recetas.controller';
 import { RecetasService } from './recetas.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Postre } from 'src/postres/entities/postre.entity';
+import { Receta } from './entities/receta.entity';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 describe('RecetasController', () => {
   let controller: RecetasController;
@@ -9,6 +14,18 @@ describe('RecetasController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RecetasController],
       providers: [RecetasService],
+      imports:[TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [Postre, Receta],
+        synchronize: true,
+      }),
+      TypeOrmModule.forFeature([Receta]),
+    ] 
     }).compile();
 
     controller = module.get<RecetasController>(RecetasController);
